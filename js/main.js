f2692d3b8f435f01b62762143074fb10da823501
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-07-15 23:04:45 +0200

    ostateczne

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function spr(n) {
+
+    if (n >= 100 && n <=200) {
+        console.log('Liczba znajduje się w przedziale');
+    } else {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+
+    return n;
+}
+
+console.log(spr(58));
